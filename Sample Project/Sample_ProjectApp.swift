705c12b835d2b5c93447d6d3143f0723186eef03
Dishant Nagpal <dishantnagpal10@gmail.com>
2022-06-17 13:37:11 +0530

    Initial Commit

---
+++ Sample Project/Sample_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sample_ProjectApp.swift
+//  Sample Project
+//
+//  Created by Dishant Nagpal on 17/06/22.
+//
+
+import SwiftUI
+
+@main
+struct Sample_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
